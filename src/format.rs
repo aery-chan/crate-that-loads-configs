aba9e29994caf7a03f8ff97b8cfbe25626252214
Aery <aeryowo@gmail.com>
2019-09-09 18:34:37 +0200

    Format trait

---
+++ src/format.rs
@@ -0,0 +1,7 @@
+pub trait Format<Read, Write, Defaults> {
+
+    fn read(input: Vec<u8>, defaults: Defaults) -> Read;
+
+    fn write(input: Write) -> Vec<u8>;
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
+pub mod format;
+
+fn main() {}
